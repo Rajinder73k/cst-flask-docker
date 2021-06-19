c20b1bd5cf092655d51e93cffffc2e527854fcbf
jasmeet singh <jasmeetsaini801@gmail.com>
2021-06-19 10:03:22 -0400

    commit

--- app.py
+++ app.py
@@ -4,4 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'CST Docker 101 abc'
+    return 'CST Docker Rajinder'
+
+@app.route('/about')
+def hello_world():
+    return 'About Rajinder'
+
+@app.route('/create')
+def hello_world():
+    return 'Create Rajinder'
+
+if __name__== '__main__':
+    app.run(host="0.0.0.0",debug=True)
